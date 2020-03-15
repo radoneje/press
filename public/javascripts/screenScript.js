56e9fb1b499eb741912b6d1d2fbfbaedee78d36a
radoneje <den.shevchenko@gmail.com>
2020-03-16 01:30:02 +0300

    f

--- public/javascripts/screenScript.js
+++ public/javascripts/screenScript.js
@@ -36,7 +36,7 @@ new Vue({
             console.log("StartShowUploadedVideo 2", data)
             var video=document.createElement('video')
 
-            video.loadedmetadata=function(){connsole.log("load metadata");video.play()}
+            video.onloadedmetadata=function(){connsole.log("load metadata");video.play()}
             video.onplaying=function(){video.classList.add("active")}
             video.preload="metadata";
             video.onended=function(){video.parentNode.removeChild(video)}
