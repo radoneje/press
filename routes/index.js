c92660d3791abb81ac312f37886f6c13097f927c
radoneje <den.shevchenko@gmail.com>
2020-03-17 18:51:51 +0300

    fd

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ router.get('/', login, async(req, res, next) =>{
   var r=await req.knex.select("*").from("t_descr")
   res.render('index', { title: r[0].title.replace("<br>",""), descr:r[0], user:req.session['user']});
       // res.render('login', { title: r[0].title, descr:r[0], user:req.session['user']});
+
 });
 
 function login(req, res, next){
