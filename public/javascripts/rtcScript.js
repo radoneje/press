d4e7b618b2b4525ef7941039dfc90a102a3a32fa
radoneje <den.shevchenko@gmail.com>
2020-03-25 17:25:21 +0300

    fdg ggf

--- public/javascripts/rtcScript.js
+++ public/javascripts/rtcScript.js
@@ -4,7 +4,7 @@ async function   initCam (_this){
     const constraints={
         audio: true,
         video: {
-            width: { min: 320, ideal: 640, max: 1280 },
+            width: { min: 320, ideal: 640, max: 1920 },
             //height:{min: 180, ideal:320 , max:720},
             facingMode: "user",
             aspectRatio: 1.777777778
